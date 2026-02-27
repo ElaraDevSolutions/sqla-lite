e51813ccd03e94285c6f434d0f64cf4792368a9d
Marcos Rosa <marcos.cantor@gmail.com>
2026-02-27 23:47:04 +0000

    chore: configure PyPI publish workflow for v1.0.0

--- sqla_lite/__init__.py
+++ sqla_lite/__init__.py
@@ -1,3 +1,5 @@
 from .core import table, Id, Size, Decimal, DateFormatMarker as DateFormat, repository, configure_database
 
-__all__ = ["table", "Id", "Size", "Decimal", "DateFormat", "repository", "configure_database"]
+__version__ = "1.0.0"
+
+__all__ = ["table", "Id", "Size", "Decimal", "DateFormat", "repository", "configure_database", "__version__"]

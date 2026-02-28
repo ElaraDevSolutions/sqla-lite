4a4c3ccd3697aef80b2f6205138bb859a071890b
Marcos Rosa <marcos.cantor@gmail.com>
2026-02-28 00:23:35 +0000

    fix: repair version files and harden version updater

--- sqla_lite/__init__.py
+++ sqla_lite/__init__.py
@@ -1,3 +1,5 @@
 from .core import table, Id, Size, Decimal, DateFormatMarker as DateFormat, repository, configure_database
 
-__version__ = "1.0.3"__all__ = ["table", "Id", "Size", "Decimal", "DateFormat", "repository", "configure_database", "__version__"]
+__version__ = "1.0.3"
+
+__all__ = ["table", "Id", "Size", "Decimal", "DateFormat", "repository", "configure_database", "__version__"]
